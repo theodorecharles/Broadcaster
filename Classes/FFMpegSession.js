68f728aa09e9d0d7ad9f5d32a36a75865b9de586
Ted Charles <me@tedcharles.net>
2025-11-29 10:42:50 -0500

    Add pm2 support and fix video codec compatibility
    - Add ecosystem.config.js for pm2 process management
    - Fix FFmpeg H.264 encoding for browser compatibility (add profile:v main, level 3.1, yuv420p)
    - Make channel buttons load dynamically from manifest.json
    - Update hls.js API calls to current syntax
    - Add Star Trek TV channel with 882 episodes
    - Update author to Ted Charles
    - Add pm2 scripts to package.json

--- Classes/FFMpegSession.js
+++ Classes/FFMpegSession.js
@@ -1,10 +1,11 @@
 const Log = require('../Utilities/Log.js')
-const FFMpeg = require('fluent-ffmpeg')
+const { spawn } = require('child_process')
 const { CACHE_DIR } = process.env
 const tag = 'FFMpegSession'
 
 const { VIDEO_CODEC,
         VIDEO_CRF,
+        VIDEO_PRESET,
         VIDEO_FILTER,
         AUDIO_CODEC,
         AUDIO_BITRATE,
@@ -15,39 +16,66 @@ function FFMpegSession(channel) {
     const file = channel.queue[channel.currentPlaylistIndex]
     const slug = channel.slug
     const start = Date.now()
-    FFMpeg(file)
-        .videoCodec(VIDEO_CODEC)
-        .videoFilters([VIDEO_FILTER])
-        .size(DIMENSIONS)
-        .audioCodec(AUDIO_CODEC)
-        .audioBitrate(AUDIO_BITRATE)
-        .audioChannels(2)
-        .format('hls')
-        .addOptions([
-            `-crf ${VIDEO_CRF}`,
-            `-hls_time ${HLS_SEGMENT_LENGTH_SECONDS}`,
-            '-hls_flags append_list',
-            '-hls_start_number_source datetime',
-            '-hls_playlist_type event'
-        ]).output(`${CACHE_DIR}/broadcaster/channels/${slug}/_.m3u8`)
-        .on('start', function(err, stdout, stderr) {
-            if (channel.currentPlaylistIndex == 0) channel.startTime = Date.now()
-            Log(tag, `FFMpeg started encoding ${file}.`, channel)
-        })
-        .on('end', (stdout, stderr) => {
+    const output = `${CACHE_DIR}/broadcaster/channels/${slug}/_.m3u8`
+
+    const args = [
+        '-i', file,
+        '-vf', VIDEO_FILTER,
+        '-s', DIMENSIONS,
+        '-c:v', VIDEO_CODEC,
+        '-preset', VIDEO_PRESET,
+        '-crf', VIDEO_CRF,
+        '-profile:v', 'main',
+        '-level', '3.1',
+        '-pix_fmt', 'yuv420p',
+        '-c:a', AUDIO_CODEC,
+        '-b:a', AUDIO_BITRATE,
+        '-ac', '2',
+        '-hls_time', HLS_SEGMENT_LENGTH_SECONDS,
+        '-hls_flags', 'append_list',
+        '-hls_start_number_source', 'datetime',
+        '-hls_playlist_type', 'event',
+        '-f', 'hls',
+        output
+    ]
+
+    const ffmpeg = spawn('ffmpeg', args)
+
+    // Log start
+    if (channel.currentPlaylistIndex == 0) channel.startTime = Date.now()
+    Log(tag, `FFMpeg started encoding ${file}.`, channel)
+
+    let stderrData = ''
+
+    ffmpeg.stdout.on('data', (data) => {
+        // FFmpeg outputs to stderr, not stdout
+    })
+
+    ffmpeg.stderr.on('data', (data) => {
+        stderrData += data.toString()
+    })
+
+    ffmpeg.on('close', (code) => {
+        if (code === 0) {
             channel.currentPlaylistIndex++
             Log(tag, `FFMpeg finished encoding ${file} in ${(Date.now() - start)/1000} seconds.`, channel)
-        })
-        .on('error', function (err, stdout, stderr) {
-            Log(tag, `FFMpeg produced an error, so we're skipping to ${file}.`, channel)
-            Log(tag, err, channel)
-            Log(tag, stderr, channel)
-            Log(tag, stdout, channel)
+        } else {
+            Log(tag, `FFMpeg produced an error (exit code ${code}), so we're skipping to next file.`, channel)
+            Log(tag, `Error output: ${stderrData}`, channel)
             channel.currentPlaylistIndex++
-            channel.segmenter.advance()
-        })
-        .run()
+            if (channel.segmenter) {
+                channel.segmenter.advance()
+            }
+        }
+    })
 
+    ffmpeg.on('error', (err) => {
+        Log(tag, `Failed to start FFMpeg: ${err.message}`, channel)
+        channel.currentPlaylistIndex++
+        if (channel.segmenter) {
+            channel.segmenter.advance()
+        }
+    })
 }
 
 module.exports = {
---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [{
+    name: 'broadcaster',
+    script: 'Broadcaster.js',
+    cwd: '/home/ted/Broadcaster',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production'
+    },
+    error_file: './logs/err.log',
+    out_file: './logs/out.log',
+    log_file: './logs/combined.log',
+    time: true,
+    merge_logs: true,
+    kill_timeout: 5000
+  }]
+};
